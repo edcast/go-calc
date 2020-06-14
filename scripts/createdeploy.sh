ad2c9b5e5a9a760481b1301967a81ed9d08a2d7f
Naveen <nair.09@gmail.com>
2020-06-14 09:59:01 +0000

    Main commit

--- scripts/createdeploy.sh
+++ scripts/createdeploy.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
+
+##########CHECK IF PREVIOUS VERSION OF APP IS RUNNING#########
+
+
+PODSTATUS=`kubectl get pods/gocalc-app | awk 'NR == 2 {print $3}'`
+
+if [ $PODSTATUS == "Running" ]
+
+then
+
+echo "Termionating the pod"
+
+kubectl delete pods/gocalc-app
+
+sleep 15
+
 kubectl apply -f /tmp/deploy.yaml
+
+else
+
+kubectl apply -f /tmp/deploy.yaml
+
+fi
+
+###WAIT FOR THE POD TO BE IN RUNNING STATE####
+
+sleep 10
